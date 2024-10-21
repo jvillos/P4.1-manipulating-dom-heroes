629dc7ffb7971382c34ed3400d575e051514d438
jvillos <jvillosl14@gmail.com>
2024-10-21 12:23:40 +0200

    developing accordeon

--- jspractica.js
+++ jspractica.js
@@ -22,13 +22,20 @@ function renderCards(jsondata) {
   for (let char of jsondata.data.results) {
     let newItem = accordeonTemplate.cloneNode(true);
 
+
+    //i want to extract name, image, description, series and comics
+
     newItem.querySelector(".accordion-button").innerHTML = char.name;
+    newItem.querySelector(".description").innerHTML = char.description;
+    newItem.querySelector(".card-img-top").src = `${char.thumbnail.path}.${char.thumbnail.extension}`;
+    /*newItem.querySelector(".accordion-collapse").id = char.id;
+    newItem.querySelector(".accordion-button").data-bs-target = `#${char.id}`;
     newItem.querySelector(".accordion-body").innerHTML = char.description;
+    newItem.querySelector(".accordion-body").innerHTML = char.description;*/
 
     items.append(newItem);
   }
 }
-
 renderCards(jsondata);
 
 
